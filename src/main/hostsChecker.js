518c42331e1e8fc608fcf015ac808aa45c2eb65e
zelkun <moeunsu@gmail.com>
2025-04-28 20:35:33 +0900

    path

--- src/main/hostsChecker.js
+++ src/main/hostsChecker.js
@@ -9,7 +9,7 @@ import pathManager from './pathManager.js';
 
 // hosts 파일 경로 (Windows 기준)
 const hostsPath = join(process.env.SYSTEMROOT, 'System32', 'drivers', 'etc', 'hosts');
-const batFilePath = join(pathManager.appRootPath, isDev ? 'build' : 'resources', 'host.bat');
+const batFilePath = join(pathManager.appRootPath, isDev ? 'build' : 'resources', 'hosts.bat');
 const domain = 'git.electron.vite';
 let mainWindow = null;
 
